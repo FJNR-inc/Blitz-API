a32e607e8ba27a9117cf327004f4bc99185b319c
RignonNoel <rignon.noel@gmail.com>
2018-04-19 14:23:59 -0400

    allow administrator to manage organisations and domain names

--- blitz_api/admin.py
+++ blitz_api/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Organization, Domain
+
+from django.utils.translation import ugettext_lazy as _
+
+
+class DomainInline(admin.StackedInline):
+    model = Domain
+    can_delete = True
+    verbose_name_plural = _('Domains')
+    fk_name = 'organization'
+    extra = 0  # No one extra blank field in the admin representation
+
+
+class CustomOrganizationAdmin(admin.ModelAdmin):
+    inlines = (DomainInline, )
+
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Organization, CustomOrganizationAdmin)
+admin.site.register(Domain)
