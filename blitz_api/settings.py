8dbc2cd39ac744e215ab93c581c3db9e61b2c83f
DarkFranX <frank.jeanneau@gmail.com>
2018-05-06 14:42:02 -0400

    Add CORS headers middleware

--- blitz_api/settings.py
+++ blitz_api/settings.py
@@ -51,7 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
+    'django.middleware.locale.LocaleMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'blitz_api.urls'
