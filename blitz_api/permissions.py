172f7571d9d82064414ccb97626f1adf5f944a5f
DarkFranX <frank.jeanneau@gmail.com>
2018-05-12 23:42:32 -0400

    Add AcademicLevel/Field & Organization viewset tests

--- blitz_api/permissions.py
+++ blitz_api/permissions.py
@@ -34,10 +34,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow admins to edit the object.
+    Custom permission to only allow admins to modify objects.
     """
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
