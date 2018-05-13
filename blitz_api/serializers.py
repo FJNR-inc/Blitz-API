1dff54b0223f88c5dbb0bd02b545b967653e16a3
DarkFranX <frank.jeanneau@gmail.com>
2018-05-13 11:40:24 -0400

    Add UserUpdateSerializer for update actions & cleanup CustomAuthToken

--- blitz_api/serializers.py
+++ blitz_api/serializers.py
@@ -64,24 +64,14 @@ class AcademicFieldSerializer(serializers.HyperlinkedModelSerializer):
         fields = '__all__'
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserUpdateSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Set  certain fields such as university, academic_level and email to read
+    only.
+    """
     id = serializers.ReadOnlyField()
     username = serializers.HiddenField(default=None)
     new_password = serializers.CharField(max_length=128, required=False)
-    email = serializers.EmailField(
-        label=_('Email address'),
-        max_length=254,
-        required=True,
-        validators=[
-            UniqueValidator(
-                queryset=User.objects.all(),
-                message=_(
-                    "An account for the specified email "
-                    "address already exists."
-                ),
-            ),
-        ],
-    )
     phone = serializers.CharField(
         allow_blank=True,
         allow_null=True,
@@ -96,9 +86,109 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         max_length=17,
         required=False,
     )
+    academic_field = AcademicFieldSerializer()
+    university = OrganizationSerializer(read_only=True)
+    academic_level = AcademicLevelSerializer(read_only=True)
+
+    def validate_academic_field(self, value):
+        """
+        Check that the academic field exists.
+        """
+        if 'name' in value:
+            field = AcademicField.objects.filter(name=value['name'])
+
+            if field:
+                return field[0]
+        raise serializers.ValidationError(
+            _("This academic field does not exist.")
+        )
+
+    def validate_phone(self, value):
+        return phone_number_validator(value)
+
+    def validate_other_phone(self, value):
+        return phone_number_validator(value)
+
+    def update(self, instance, validated_data):
+        validated_data['username'] = instance.username
+        if 'new_password' in validated_data.keys():
+            try:
+                old_pw = validated_data.pop('password')
+            except KeyError:
+                raise serializers.ValidationError({
+                    'password': _("This field is required.")
+                })
+
+            new_pw = validated_data.pop('new_password')
+
+            try:
+                password_validation.validate_password(password=new_pw)
+            except ValidationError as err:
+                raise serializers.ValidationError({
+                    'new_password': err.messages
+                })
+
+            if instance.check_password(old_pw):
+                instance.set_password(new_pw)
+                instance.save()
+            else:
+                msg = {'password': _("Bad password")}
+                raise serializers.ValidationError(msg)
+
+        return super().update(instance, validated_data)
+
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'new_password': {'write_only': True},
+            'gender': {
+                'allow_blank': False,
+            },
+            'first_name': {
+                'allow_blank': False,
+            },
+            'last_name': {
+                'allow_blank': False,
+            },
+        }
+        read_only_fields = (
+            'id',
+            'url',
+            'is_staff',
+            'is_superuser',
+            'is_active',
+            'date_joined',
+            'last_login',
+            'groups',
+            'user_permissions',
+            'university',
+            'academic_level',
+            'email',
+        )
+
+
+class UserSerializer(UserUpdateSerializer):
+    """
+    Complete serializer for user creation
+    """
+    email = serializers.EmailField(
+        label=_('Email address'),
+        max_length=254,
+        required=True,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message=_(
+                    "An account for the specified email "
+                    "address already exists."
+                ),
+            ),
+        ],
+    )
     university = OrganizationSerializer()
     academic_level = AcademicLevelSerializer()
-    academic_field = AcademicFieldSerializer()
 
     def validate_university(self, value):
         """
@@ -124,25 +214,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             _("This academic level does not exist.")
         )
 
-    def validate_academic_field(self, value):
-        """
-        Check that the academic field exists.
-        """
-        if 'name' in value:
-            field = AcademicField.objects.filter(name=value['name'])
-
-            if field:
-                return field[0]
-        raise serializers.ValidationError(
-            _("This academic field does not exist.")
-        )
-
-    def validate_phone(self, value):
-        return phone_number_validator(value)
-
-    def validate_other_phone(self, value):
-        return phone_number_validator(value)
-
     def validate_password(self, value):
         try:
             password_validation.validate_password(password=value)
@@ -150,29 +221,24 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             raise serializers.ValidationError(err.messages)
         return value
 
-    def validate_new_password(self, value):
-        try:
-            password_validation.validate_password(password=value)
-        except ValidationError as err:
-            raise serializers.ValidationError(err.messages)
-        return value
+    def validate(self, attrs):
+        if 'email' in attrs:
+            attrs['username'] = attrs['email']
+        return attrs
 
     def create(self, validated_data):
         """
         Check that the email domain correspond to the university.
         """
-        try:
-            domains = Organization.objects.get(
-                name=validated_data['university'].name
-            ).domains.all()
-        except Organization.DoesNotExist:
-            raise serializers.ValidationError(
-                _("This university does not exist.")
-            )
+        domains = Organization.objects.get(
+            name=validated_data['university'].name
+        ).domains.all()
 
         email_domain = validated_data['email'].split("@", 1)[1]
         if not any(d.name == email_domain for d in domains):
-            raise serializers.ValidationError(_("Invalid domain name."))
+            raise serializers.ValidationError({
+                'email': [_("Invalid domain name.")]
+            })
 
         user = User(**validated_data)
 
@@ -192,35 +258,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
         return user
 
-    def validate(self, attrs):
-        if 'email' in attrs:
-            attrs['username'] = attrs['email']
-        return attrs
-
-    def update(self, instance, validated_data):
-        # Drop keys that cannot be updated. None ensures that no exception is
-        # raised if the key doesn't exist.
-        validated_data.pop("university", None)
-        validated_data.pop("email", None)
-        validated_data.pop("academic_level", None)
-        if 'new_password' in validated_data.keys():
-            try:
-                old_pw = validated_data.pop('password')
-            except KeyError:
-                raise serializers.ValidationError(
-                    _("This field is required.")
-                )
-
-            new_pw = validated_data.pop('new_password')
-
-            if instance.check_password(old_pw):
-                instance.set_password(new_pw)
-            else:
-                msg = _("Bad password.")
-                raise serializers.ValidationError(msg)
-
-        return super().update(instance, validated_data)
-
     class Meta:
         model = User
         fields = '__all__'
@@ -230,10 +267,21 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             'university': {'required': True},
             'academic_level': {'required': True},
             'academic_field': {'required': True},
-            'gender': {'required': True},
-            'first_name': {'required': True},
-            'last_name': {'required': True},
-            'birthdate': {'required': True},
+            'gender': {
+                'required': True,
+                'allow_blank': False,
+            },
+            'first_name': {
+                'required': True,
+                'allow_blank': False,
+            },
+            'last_name': {
+                'required': True,
+                'allow_blank': False,
+            },
+            'birthdate': {
+                'required': True,
+            },
         }
         read_only_fields = (
             'id',
@@ -252,33 +300,29 @@ class CustomAuthTokenSerializer(AuthTokenSerializer):
     """
     Subclass of default AuthTokenSerializer to enable email authentication
     """
-    username = serializers.CharField(label=_("Username"))
+    username = serializers.CharField(label=_("Username"), required=True)
     password = serializers.CharField(
         label=_("Password"),
         style={'input_type': 'password'},
-        trim_whitespace=False
+        trim_whitespace=False,
+        required=True,
     )
 
     def validate(self, attrs):
         username = attrs.get('username')
         password = attrs.get('password')
 
-        if username and password:
-            try:
-                user_obj = User.objects.get(email=username)
-                if user_obj:
-                    username = user_obj.username
-            except User.DoesNotExist:
-                pass
+        try:
+            user_obj = User.objects.get(email=username)
+            username = user_obj.username
+        except User.DoesNotExist:
+            pass
 
-            user = authenticate(request=self.context.get('request'),
-                                username=username, password=password)
+        user = authenticate(request=self.context.get('request'),
+                            username=username, password=password)
 
-            if not user:
-                msg = _('Unable to log in with provided credentials.')
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = _('Must include "username" and "password".')
+        if not user:
+            msg = _('Unable to log in with provided credentials.')
             raise serializers.ValidationError(msg, code='authorization')
 
         attrs['user'] = user
