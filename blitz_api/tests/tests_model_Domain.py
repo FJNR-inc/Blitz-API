50b086167d20ba3ffb58c102b142ad99a1ae411d
DarkFranX <frank.jeanneau@gmail.com>
2018-05-12 22:11:20 -0400

    Add Domain model tests

---
+++ blitz_api/tests/tests_model_Domain.py
@@ -0,0 +1,37 @@
+from rest_framework.test import APITestCase
+
+from ..models import Domain, Organization
+
+
+class DomainTests(APITestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(DomainTests, cls).setUpClass()
+        cls.org = Organization.objects.create(name="random_university")
+
+    def test_create(self):
+        """
+        Ensure that we can create a domain with a valid organization.
+        """
+        domain = Domain.objects.create(
+            name="random_domain",
+            organization_id=self.org.id
+        )
+
+        self.assertEquals(domain.name, "random_domain")
+
+    def test_create_invalid_organization(self):
+        """
+        Ensure that we can create a domain with an invalid organization.
+        """
+        domain = Domain.objects.create(
+            name="random_domain",
+            organization_id=9999
+        )
+
+        try:
+            domain.organization
+            return False
+        except Organization.DoesNotExist:
+            return True
