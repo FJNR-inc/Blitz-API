ba1c77e427baf7f32aa0504f68501f8424cc1199
jerome <jeffyer38130@gmail.com>
2019-05-26 21:38:19 +0200

    Add test for export Reformat setting file

--- blitz_api/settings.py
+++ blitz_api/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'simple_history.middleware.HistoryRequestMiddleware',
-    'request_logging.middleware.LoggingMiddleware', # logs requests body
+    'request_logging.middleware.LoggingMiddleware',  # logs requests body
 ]
 
 # django-request-logging settings
@@ -133,7 +133,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blitz_api.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -171,7 +170,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -191,16 +189,21 @@ LANGUAGES = [
 ]
 
 # File upload size
-DATA_UPLOAD_MAX_MEMORY_SIZE = config('DATA_UPLOAD_MAX_MEMORY_SIZE', default=2621440, cast=int)
-FILE_UPLOAD_MAX_MEMORY_SIZE = config('FILE_UPLOAD_MAX_MEMORY_SIZE', default=2621440, cast=int)
-
+DATA_UPLOAD_MAX_MEMORY_SIZE = config('DATA_UPLOAD_MAX_MEMORY_SIZE',
+                                     default=2621440, cast=int)
+FILE_UPLOAD_MAX_MEMORY_SIZE = config('FILE_UPLOAD_MAX_MEMORY_SIZE',
+                                     default=2621440, cast=int)
 
 # AWS Deployment configuration (with Zappa)
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='ca-central-1')
-AWS_STORAGE_STATIC_BUCKET_NAME = config('AWS_STORAGE_STATIC_BUCKET_NAME', default='example_static')
-AWS_STORAGE_MEDIA_BUCKET_NAME = config('AWS_STORAGE_MEDIA_BUCKET_NAME', default='example_media')
-AWS_S3_STATIC_CUSTOM_DOMAIN = config('AWS_S3_STATIC_CUSTOM_DOMAIN', default='example_static.s3.region.amazonaws.com')
-AWS_S3_MEDIA_CUSTOM_DOMAIN = config('AWS_S3_MEDIA_CUSTOM_DOMAIN', default='example_media.s3.region.amazonaws.com')
+AWS_STORAGE_STATIC_BUCKET_NAME = config('AWS_STORAGE_STATIC_BUCKET_NAME',
+                                        default='example_static')
+AWS_STORAGE_MEDIA_BUCKET_NAME = config('AWS_STORAGE_MEDIA_BUCKET_NAME',
+                                       default='example_media')
+AWS_S3_STATIC_CUSTOM_DOMAIN = config('AWS_S3_STATIC_CUSTOM_DOMAIN',
+                                     default='example_static.s3.region.amazonaws.com')
+AWS_S3_MEDIA_CUSTOM_DOMAIN = config('AWS_S3_MEDIA_CUSTOM_DOMAIN',
+                                    default='example_media.s3.region.amazonaws.com')
 AWS_S3_STATIC_DIR = config('AWS_S3_STATIC_DIR', default='static')
 AWS_S3_MEDIA_DIR = config('AWS_S3_MEDIA_DIR', default='media')
 
@@ -212,18 +215,15 @@ if len(sys.argv) > 1 and sys.argv[1] == 'test':
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 else:
     STATIC_URL = config('STATIC_URL', default='/static/')
-    STATICFILES_STORAGE = config('STATICFILES_STORAGE', default='django.contrib.staticfiles.storage.StaticFilesStorage')
+    STATICFILES_STORAGE = config('STATICFILES_STORAGE',
+                                 default='django.contrib.staticfiles.storage.StaticFilesStorage')
 
 # User uploaded files (MEDIA)
-# Force local storage for unittests. Temporary.
-if len(sys.argv) > 1 and sys.argv[1] == 'test':
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = 'media/'
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-else:
-    MEDIA_URL = config('MEDIA_URL', default='/media/')
-    MEDIA_ROOT = config('MEDIA_ROOT', default='media/')
-    DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE', default='django.core.files.storage.FileSystemStorage')
+MEDIA_URL = config('MEDIA_URL', default='/media/')
+MEDIA_ROOT = config('MEDIA_ROOT', default='media/')
+DEFAULT_FILE_STORAGE = config(
+    'DEFAULT_FILE_STORAGE',
+    default='django.core.files.storage.FileSystemStorage')
 
 # Django Rest Framework
 
@@ -247,19 +247,19 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100
 }
 
-
 # CORS Header Django Rest Framework
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_EXPOSE_HEADERS = ["Link",]
-
+CORS_EXPOSE_HEADERS = ["Link", ]
 
 # Temporary Token
 
 REST_FRAMEWORK_TEMPORARY_TOKENS = {
     'MINUTES': config('TEMPORARY_TOKEN_MINUTES', default=30, cast=int),
-    'RENEW_ON_SUCCESS': config('TEMPORARY_TOKEN_RENEW_ON_SUCCESS', default=True, cast=bool),
-    'USE_AUTHENTICATION_BACKENDS': config('USE_AUTHENTICATION_BACKENDS', default=False, cast=bool),
+    'RENEW_ON_SUCCESS': config('TEMPORARY_TOKEN_RENEW_ON_SUCCESS',
+                               default=True, cast=bool),
+    'USE_AUTHENTICATION_BACKENDS': config('USE_AUTHENTICATION_BACKENDS',
+                                          default=False, cast=bool),
 }
 
 # Activation Token
@@ -268,24 +268,27 @@ ACTIVATION_TOKENS = {
     'MINUTES': config('ACTIVATION_TOKENS_MINUTES', default=1440, cast=int),
 }
 
-
 # Email service configuration (using Anymail).
 # Refer to Anymail's documentation for configuration details.
 
 ANYMAIL = {
     'SENDINBLUE_API_KEY': config('SENDINBLUE_API_KEY', default='example_key'),
-    'REQUESTS_TIMEOUT': config('REQUESTS_TIMEOUT', default=(30, 30), cast=tuple),
+    'REQUESTS_TIMEOUT': config('REQUESTS_TIMEOUT', default=(30, 30),
+                               cast=tuple),
     'TEMPLATES': {
         'CONFIRM_SIGN_UP': config('CONFIRM_SIGN_UP', default='example_id'),
         'FORGOT_PASSWORD': config('FORGOT_PASSWORD', default='example_id'),
-        'RESERVATION_CANCELLED': config('RESERVATION_CANCELLED', default='example_id'),
-        'CONFIRM_CHANGE_EMAIL': config('CONFIRM_CHANGE_EMAIL', default='example_id'),
+        'RESERVATION_CANCELLED': config('RESERVATION_CANCELLED',
+                                        default='example_id'),
+        'CONFIRM_CHANGE_EMAIL': config('CONFIRM_CHANGE_EMAIL',
+                                       default='example_id'),
     },
 }
-EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND = config('EMAIL_BACKEND',
+                       default='django.core.mail.backends.smtp.EmailBackend')
 # This 'FROM' email is not used with SendInBlue templates
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@example.org')
-
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL',
+                            default='noreply@example.org')
 
 # Django email service. Used for administrative emails.
 
@@ -295,23 +298,27 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='example@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='password')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 # Email addresses to notify in case of error
-ADMINS = config('ADMINS', default="", cast=lambda v: literal_eval("["+v+"]"))
+ADMINS = config('ADMINS', default="",
+                cast=lambda v: literal_eval("[" + v + "]"))
 SERVER_EMAIL = config('SERVER_EMAIL', default='example@gmail.com')
 SUPPORT_EMAIL = config('SUPPORT_EMAIL', default='admin@fjnr.ca')
-
 
 # User specific settings
 
 LOCAL_SETTINGS = {
     'ORGANIZATION': config('ORGANIZATION', default='Blitz'),
     'EMAIL_SERVICE': config('EMAIL_SERVICE', default=False, cast=bool),
-    'AUTO_ACTIVATE_USER': config('AUTO_ACTIVATE_USER', default=False, cast=bool),
+    'AUTO_ACTIVATE_USER': config('AUTO_ACTIVATE_USER', default=False,
+                                 cast=bool),
     'FRONTEND_INTEGRATION': {
-        'ACTIVATION_URL': config('ACTIVATION_URL', default='https://example.com/activate/{{token}}'),
-        'FORGOT_PASSWORD_URL': config('FORGOT_PASSWORD_URL', default='https://example.com/reset-password/{{token}}'),
+        'ACTIVATION_URL': config('ACTIVATION_URL',
+                                 default='https://example.com/activate/{{token}}'),
+        'FORGOT_PASSWORD_URL': config('FORGOT_PASSWORD_URL',
+                                      default='https://example.com/reset-password/{{token}}'),
     },
     'SELLING_TAX': 0.14975,
-    'RETIREMENT_NOTIFICATION_LIFETIME_DAYS': config('RETIREMENT_NOTIFICATION_LIFETIME_DAYS', default=30),
+    'RETIREMENT_NOTIFICATION_LIFETIME_DAYS': config(
+        'RETIREMENT_NOTIFICATION_LIFETIME_DAYS', default=30),
 }
 
 # Payment settings
@@ -320,7 +327,8 @@ PAYSAFE = {
     'ACCOUNT_NUMBER': config('PAYSAFE_ACCOUNT_NUMBER', default='1234567890'),
     'USER': config('PAYSAFE_USER', default='user'),
     'PASSWORD': config('PAYSAFE_PASSWORD', default='password'),
-    'BASE_URL': config('PAYSAFE_BASE_URL', default='https://api.test.paysafe.com/'),
+    'BASE_URL': config('PAYSAFE_BASE_URL',
+                       default='https://api.test.paysafe.com/'),
     'VAULT_URL': config('PAYSAFE_VAULT_URL', default='customervault/v1/'),
     'CARD_URL': config('PAYSAFE_CARD_URL', default='cardpayments/v1/'),
 }
@@ -328,7 +336,6 @@ PAYSAFE = {
 # django-import-export
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
-
 
 # External scheduler
 EXTERNAL_SCHEDULER = {
