e9bf55e9514c28255b9f680221dd8986ff043324
Jerome Celle <jeffyer38130@gmail.com>
2020-01-13 11:19:31 +0100

    Update admin panel with auto complete fields

--- retirement/admin.py
+++ retirement/admin.py
@@ -78,7 +78,7 @@ class ReservationAdmin(SimpleHistoryAdmin,
         'cancelation_action',
     ) + SafeDeleteAdmin.list_filter
 
-    autocomplete_fields = ['user', 'order_line', 'retreat']
+    autocomplete_fields = ['user', 'order_line', 'retreat', 'invitation']
 
     actions = ['undelete_selected', 'export_admin_action']
 
@@ -126,11 +126,53 @@ class RetreatInvitationAdmin(SimpleHistoryAdmin,
         ('coupon', admin.RelatedOnlyFieldListFilter)
     ) + SafeDeleteAdmin.list_filter
 
+    search_fields = (
+        'name', 'id',
+    )
+
+
+class WaitQueuePlaceReservedInline(admin.StackedInline):
+    model = WaitQueuePlaceReserved
+    can_delete = True
+    show_change_link = True
+    verbose_name_plural = _('Wait Queue places reserved')
+
+
+class WaitQueuePlaceAdmin(admin.ModelAdmin):
+    inlines = (WaitQueuePlaceReservedInline,)
+    list_display = (
+        'id',
+        'retreat',
+        'create',
+        'available'
+    )
+    list_filter = (
+        'retreat',
+        'cancel_by'
+    )
+    autocomplete_fields = ('cancel_by', 'retreat')
+    search_fields = ('retreat', 'id')
+
+
+class WaitQueuePlaceReservedAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'wait_queue_place',
+        'user',
+        'create',
+        'notified'
+    )
+    list_filter = (
+        'wait_queue_place',
+        'user'
+    )
+    autocomplete_fields = ('user', 'wait_queue_place')
+
 
 admin.site.register(Retreat, RetreatAdmin)
 admin.site.register(Picture, PictureAdmin)
 admin.site.register(Reservation, ReservationAdmin)
 admin.site.register(WaitQueue, WaitQueueAdmin)
 admin.site.register(RetreatInvitation, RetreatInvitationAdmin)
-admin.site.register(WaitQueuePlace)
-admin.site.register(WaitQueuePlaceReserved)
+admin.site.register(WaitQueuePlace, WaitQueuePlaceAdmin)
+admin.site.register(WaitQueuePlaceReserved, WaitQueuePlaceReservedAdmin)
