53c4bbccf964ccdfd81e7c72c8bbde3210764836
RignonNoel <noel.rignon@fjnr.ca>
2021-10-31 14:28:22 -0400

    enhance admin panel for users

--- blitz_api/admin.py
+++ blitz_api/admin.py
@@ -49,7 +49,26 @@ class CustomUserAdmin(UserAdmin, SimpleHistoryAdmin, ExportActionModelAdmin):
     """ Required to display extra fields of users in Django Admin """
     resource_class = UserResource
     form = CustomUserChangeForm
-    search_fields = ['first_name', 'last_name', 'email', 'id']
+    search_fields = [
+        'first_name',
+        'last_name',
+        'email',
+        'id',
+    ]
+    list_filter = (
+        'membership',
+        'university',
+        'is_staff',
+        'is_superuser',
+        'is_active',
+    )
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'membership',
+        'is_staff',
+    )
 
     def __init__(self, *args, **kwargs):
         super(CustomUserAdmin, self).__init__(*args, **kwargs)
