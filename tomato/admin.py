27e5e4a4b8d9e0f3f8be0e2ec3525247ea38f517
RignonNoel <noel.rignon@fjnr.ca>
2021-12-01 11:22:40 -0500

    add report on admin panel

--- tomato/admin.py
+++ tomato/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from tomato.models import (
     Message,
     Attendance,
+    Report,
 )
 
 admin.site.register(Message)
 admin.site.register(Attendance)
+admin.site.register(Report)
