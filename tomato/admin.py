cbb1170fd55be0937bcc18ba2436af1de9a6c204
RignonNoel <noel.rignon@fjnr.ca>
2021-12-01 12:03:24 -0500

    enhance report admin panel display

--- tomato/admin.py
+++ tomato/admin.py
@@ -1,3 +1,4 @@
+from admin_auto_filters.filters import AutocompleteFilter
 from django.contrib import admin
 from tomato.models import (
     Message,
@@ -5,6 +6,34 @@ from tomato.models import (
     Report,
 )
 
+
+class UserFilter(AutocompleteFilter):
+    title = 'User'
+    field_name = 'user'
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'created_at',
+        'reason',
+        'author',
+    )
+    list_filter = (
+        UserFilter,
+        'created_at',
+    )
+
+    autocomplete_fields = ['user']
+
+    @staticmethod
+    def author(report):
+        return report.message.user
+
+    class Media:
+        pass
+
+
 admin.site.register(Message)
 admin.site.register(Attendance)
-admin.site.register(Report)
+admin.site.register(Report, ReportAdmin)
