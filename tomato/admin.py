7e369592afa9eb850c57caf189501edef5c53b70
RignonNoel <noel.rignon@fjnr.ca>
2022-01-24 15:56:38 -0500

    add tomato to admin panel

--- tomato/admin.py
+++ tomato/admin.py
@@ -1,9 +1,10 @@
-from admin_auto_filters.filters import AutocompleteFilter
+from admin_auto_filters.filters import AutocompleteFilter, \
+    AutocompleteFilterFactory
 from django.contrib import admin
 from tomato.models import (
     Message,
     Attendance,
-    Report,
+    Report, Tomato,
 )
 
 
@@ -34,6 +35,21 @@ class ReportAdmin(admin.ModelAdmin):
         pass
 
 
+class TomatoAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'number_of_tomato',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        AutocompleteFilterFactory('User', 'user'),
+    )
+
+    autocomplete_fields = ['user']
+
+
 admin.site.register(Message)
 admin.site.register(Attendance)
 admin.site.register(Report, ReportAdmin)
+admin.site.register(Tomato, TomatoAdmin)
