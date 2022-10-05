5438e29147d820a30367512bdbb4e52076a6d9c1
RomainFayolle <romain.fayolle@fjnr.ca>
2022-10-05 17:28:29 -0400

    add test

--- store/tests/tests_viewset_OptionProduct.py
+++ store/tests/tests_viewset_OptionProduct.py
@@ -16,6 +16,7 @@ from rest_framework.test import (
 from blitz_api.factories import (
     UserFactory,
     AdminFactory,
+    RetreatFactory,
 )
 from retirement.models import (
     Retreat,
@@ -262,3 +263,85 @@ class OrderTests(APITestCase):
         self.options_1.refresh_from_db()
 
         self.assertFalse(self.options_1.available)
+
+    def test_option_retreat_type(self):
+        """
+        Test that when we have an option product linked to a retreat type, then all retreat
+        created with this type also have this option product.
+        """
+        self.client.force_authenticate(user=self.admin)
+        retreatType1 = RetreatType.objects.create(
+            name="New retreat type",
+            minutes_before_display_link=10,
+            number_of_tomatoes=4,
+        )
+        data = {
+            'name': "retreat type option",
+            'details': "retreat type option",
+            'available_on_retreat_types': [retreatType1.id],
+            'available': True,
+            'price': '50.00',
+            'max_quantity': 10,
+        }
+        response = self.client.post(
+            reverse('optionproduct-list'),
+            data,
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED,
+                         response.content)
+        content = json.loads(response.content)
+        option_id = content['id']
+
+        retreat1 = RetreatFactory(
+            name='retreat1',
+            seats=400,
+            display_start_time=LOCAL_TIMEZONE.localize(datetime(2130, 1, 15, 8)),
+            type=retreatType1)
+        RetreatDate.objects.create(
+            start_time=LOCAL_TIMEZONE.localize(datetime(2130, 1, 15, 8)),
+            end_time=LOCAL_TIMEZONE.localize(datetime(2130, 1, 17, 12)),
+            retreat=retreat1,
+        )
+        retreat1.activate()
+        retreat2 = RetreatFactory(
+            name='retreat2',
+            seats=400,
+            display_start_time=LOCAL_TIMEZONE.localize(datetime(2130, 1, 15, 8)),
+            type=retreatType1)
+        RetreatDate.objects.create(
+            start_time=LOCAL_TIMEZONE.localize(datetime(2130, 1, 15, 8)),
+            end_time=LOCAL_TIMEZONE.localize(datetime(2130, 1, 17, 12)),
+            retreat=retreat2,
+        )
+        retreat2.activate()
+        response = self.client.get(
+            reverse(
+                'retreat:retreat-detail',
+                kwargs={'pk': retreat1.id},
+            ),
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+            response.content
+        )
+        content = json.loads(response.content)
+        self.assertEqual(content.get('options')[0].get('id'),
+                         option_id,
+                         content)
+        response = self.client.get(
+            reverse(
+                'retreat:retreat-detail',
+                kwargs={'pk': retreat2.id},
+            ),
+        )
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+            response.content
+        )
+        content = json.loads(response.content)
+        self.assertEqual(content.get('options')[0].get('id'),
+                         option_id,
+                         content)
