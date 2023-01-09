eac415e7e4eb88e595eb8dd77035d88a9a9cbf25
RomainFayolle <romain.fayolle@fjnr.ca>
2023-01-09 16:56:08 -0500

    correct test for timeslot

--- workplace/tests/tests_model_TimeSlot.py
+++ workplace/tests/tests_model_TimeSlot.py
@@ -96,7 +96,7 @@ class TimeSlotTests(APITestCase):
             3600*4 + 60*47
         )
 
-    def test_property_number_of_tomato(self):
+    def test_property_number_of_tomatoes(self):
         time_slot = TimeSlot.objects.create(
             name="random_time_slot",
             period=self.period,
@@ -105,6 +105,6 @@ class TimeSlotTests(APITestCase):
             end_time=timezone.now() + timedelta(hours=4, minutes=47),
         )
         self.assertEqual(
-            time_slot.number_of_tomato,
+            time_slot.number_of_tomatoes,
             time_slot.duration // 3600
         )
