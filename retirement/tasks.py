1799c22e046381adcd915985315df7a3aab16fd0
RomainFayolle <romain.fayolle@fjnr.ca>
2023-01-13 13:55:18 -0500

    update retreat reservation tomato crediting to avoid duplicates

--- retirement/tasks.py
+++ retirement/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.db import transaction
 from django.db.models import Q
 from django.utils import timezone
+from django.contrib.contenttypes.models import ContentType
 
 
 @shared_task
@@ -9,7 +10,9 @@ def assign_retreat_tomatoes():
     """
     Assign tomatoes to all active users of a retreat if one of the date passed.
     """
-    from retirement.models import RetreatDate
+    from retirement.models import RetreatDate, Reservation
+    reservation_type = ContentType.objects.get_for_model(
+        Reservation)
     today = timezone.now()
     to_assign_dates = RetreatDate.objects.filter(
         end_time__lte=today,
@@ -22,11 +25,12 @@ def assign_retreat_tomatoes():
                 Q(is_active=True) | Q(cancelation_date__gte=date.end_time))
             for reservation in active_reservations:
                 from tomato.models import Tomato
-                Tomato.objects.create(
+                Tomato.objects.get_or_create(
                     user=reservation.user,
                     number_of_tomato=date.number_of_tomatoes,
                     source=Tomato.TOMATO_SOURCE_RETREAT,
-                    content_object=reservation,
+                    content_type=reservation_type,
+                    object_id=reservation.id,
                 )
             date.tomatoes_assigned = True
             date.save()
