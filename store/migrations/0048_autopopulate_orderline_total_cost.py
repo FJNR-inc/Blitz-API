53cc3be19fae2e9c8cc49d7dbeb282577cdbcd14
RomainFayolle <romain.fayolle@fjnr.ca>
2023-01-24 10:40:21 -0500

    use content type to access foreign model and avoid Attribute Error

--- store/migrations/0048_autopopulate_orderline_total_cost.py
+++ store/migrations/0048_autopopulate_orderline_total_cost.py
@@ -4,14 +4,17 @@ from django.db import migrations
 
 
 def populate_orderline_total_cost(apps, schema_editor):
+    from django.contrib.contenttypes.models import ContentType
     OrderLine = apps.get_model('store', 'OrderLine')
 
     order_lines = OrderLine.objects.all()
     for line in order_lines:
         # total_cost is the value of old cost.
         line.total_cost = line.cost
-        base = line.content_object
-        base_price = base.price if base.price else 0
+        ct = ContentType.objects.get(model=line.content_type.model,
+                                     app_label=line.content_type.app_label)
+        content_object = ct.get_object_for_this_type(pk=line.object_id)
+        base_price = content_object.price if content_object.price else 0
         coupon_value = line.coupon_real_value if line.coupon_real_value else 0
         line.cost = base_price - coupon_value
         # No need to apply coupon on total_cost for it copies old cost
