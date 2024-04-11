cd330ceed9d14754e1308ad282c97cfcf10ec63d
RignonNoel <noel.rignon@fjnr.ca>
2024-04-10 20:06:01 -0400

    add status report on celery tasks

--- cron_manager/tasks.py
+++ cron_manager/tasks.py
@@ -1,4 +1,6 @@
 from celery import shared_task
+from django.conf import settings
+import requests
 
 
 @shared_task
@@ -6,3 +8,13 @@ def trigger_task_executions():
     from .cron_function import execute_tasks
 
     execute_tasks()
+
+    try:
+        status_url = settings.LOCAL_SETTINGS['STATUS_URLS']['TASK_EXECUTION']
+
+        if status_url:
+            requests.get(status_url)
+    except Exception:
+        # We don't want to block the task because of a status update
+        # Status system should already report the error if needed
+        pass
