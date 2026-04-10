99a3a2471ff835dc8080bceb9efd11868b14d012
Noel Rignon <noel.rignon@fjnr.ca>
2026-04-10 13:23:10 -0400

    remove unused import

--- blitz_api/urls.py
+++ blitz_api/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import path
